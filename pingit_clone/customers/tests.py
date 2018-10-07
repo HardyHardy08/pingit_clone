8b7f8c82bfa95033c579eeb3917f4d2026039ed6
Hardy <hardiansyah.iiI@gmail.com>
2018-10-07 13:11:04 +0700

    update gitignore file and add test file for customers

--- pingit_clone/customers/tests.py
+++ pingit_clone/customers/tests.py
@@ -1,2 +1,19 @@
-from django.test import TestCase
+from selenium import webdriver
+import unittest
 
+
+class NewUserTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_it_worked(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn('Welcome to Django', self.browser.title)
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
