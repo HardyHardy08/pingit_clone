42468819df3d982cfc28f446839f281b0d62be90
Hardy <hardiansyah.iiI@gmail.com>
2018-10-08 01:59:09 +0700

    for banking: -create model tests -update model fields

--- pingit_clone/banking/models.py
+++ pingit_clone/banking/models.py
@@ -15,36 +15,40 @@ from . import managers
 
 
 class Merchant(models.Model):
-    merchant_ID = models.IntegerField(primary_key=True, blank=False)
+    merchant_ID = models.CharField(max_length=3, blank=False)
     merchant_desc = models.CharField(max_length=50, blank=False)
 
 
-class Transaction(TimeStampedModel):
-    account_number = models.ForeignKey()
-    merchant_ID = models.ForeignKey()
-    transaction_type = models.ForeignKey()
+class AccountType(models.Model):
+    account_type_desc = models.CharField(max_length=50, blank=False)
 
-    transaction_amount = models.FloatField(default=0.0, blank=False)
-    other_details = models.CharField(max_length=140)
 
-    objects = managers.TransactionManager
+class AccountStatus(models.Model):
+    account_status_desc = models.CharField(max_length=20, blank=False)
+
+
+class Account(models.Model):
+    account_status_code = models.ForeignKey(AccountStatus, on_delete=models.PROTECT)
+    account_type_code = models.ForeignKey(AccountType, on_delete=models.PROTECT)
+    customer_id = models.ForeignKey('customers.Customer', on_delete=models.PROTECT)
+
+    account_number = models.CharField(max_length=15, primary_key=True, blank=False)
+    current_balance = models.FloatField(default=0.0)
+    other_details = models.CharField(max_length=200, blank=True)
+
+    objects = managers.AccountManager()
 
 
 class TransactionType(models.Model):
     transaction_type_desc = models.CharField(max_length=50, blank=False)
 
 
-class Account(models.Model):
-    account_status_code = models.ForeignKey()
-    account_type_code = models.ForeignKey()
-    customer_id = models.ForeignKey
+class Transaction(TimeStampedModel):
+    account_number = models.ForeignKey(Account, on_delete=models.PROTECT)
+    merchant_ID = models.ForeignKey(Merchant, on_delete=models.PROTECT)
+    transaction_type = models.ForeignKey(TransactionType, on_delete=models.PROTECT)
 
-    account_number = models.CharField(max_length=15, primary_key=True, blank=False)
-    current_balance = models.FloatField(default=0.0)
-    other_details = models.CharField(max_length=200, blank=True)
+    transaction_amount = models.FloatField(default=0.0, blank=False)
+    other_details = models.CharField(max_length=140)
 
-    objects = managers.TransactionManager
-
-
-class AccountType(models.Model):
-    account_type_desc = models.CharField(max_length=50, blank=False)
+    objects = managers.TransactionManager()
--- pingit_clone/banking/tests/test_models.py
+++ pingit_clone/banking/tests/test_models.py
@@ -1,6 +1,32 @@
 from django.core.exceptions import ValidationError
 from django.test import TestCase
-from banking.models import Transaction, Account
+from banking.models import (
+    Transaction, TransactionType, Account, AccountStatus, AccountType, Merchant
+)
+from customers.models import Customer
+from customers.tests.test_models import valid_customer_data, setup_IdentificationTypes
+
+
+def valid_account():
+    return {
+        'account_status_code': AccountStatus.objects.get(account_status_desc='Active'),
+        'account_type_code': AccountType.objects.get(account_type_desc="Savings"),
+        'customer_id': Customer.objects.get(username='JohnDoe'),
+        'account_number': '123-123-123000',
+        'current_balance': '1000.0',
+        'other_details': '',
+    }
+
+
+def create_fixtures():
+    # Customer app models
+    setup_IdentificationTypes()
+    Customer.objects.create(**valid_customer_data())
+
+    TransactionType.objects.create(transaction_type_desc="Transfer")
+    AccountStatus.objects.create(account_status_desc="Active")
+    AccountType.objects.create(account_type_desc="Savings")
+    Merchant.objects.create(merchant_ID="001", merchant_desc="Boong Bank")
 
 
 class DefaultTransactionTest(TestCase):
@@ -9,28 +35,46 @@ class DefaultTransactionTest(TestCase):
     """
 
     def setUp(self):
-        self.valid_transaction = {}
-        self.invalid_transaction = {}
+        create_fixtures()
+        account = Account.objects.create(**valid_account())
+        self.valid_transaction = {
+            'account_number': account,
+            'merchant_ID': Merchant.objects.get(merchant_ID="001"),
+            'transaction_type': TransactionType.objects.get(transaction_type_desc="Transfer"),
+            'transaction_amount': 10.0,
+            'other_details': 'August/18 rent',
+        }
+        self.invalid_transaction = {
+            'account_number': account,
+            'merchant_ID': Merchant.objects.get(merchant_ID="001"),
+            'transaction_type': TransactionType.objects.get(transaction_type_desc="Transfer"),
+            'transaction_amount': 'tree fiddy',
+            'other_details': 'August/18 rent',
+        }
 
     def test_valid_create_transaction(self):
         new_transaction = Transaction.objects.create(**self.valid_transaction)
         self.assertIsInstance(new_transaction, Transaction)
 
     def test_invalid_create_transaction(self):
-        with self.assertRaises(ValidationError):
+        with self.assertRaises(ValueError):
             Transaction.objects.create(**self.invalid_transaction)
 
     def test_valid_update_transaction(self):
         transaction = Transaction.objects.create(**self.valid_transaction)
-        transaction.merchant_ID = "5"
+        new_merchant = Merchant.objects.create(merchant_ID="002",
+                                               merchant_desc="Zonk Bank")
+        transaction.merchant_ID = new_merchant
         transaction.save()
-        self.assertEqual(transaction.merchant_ID, "5")
+        self.assertEqual(
+            Transaction.objects.get(merchant_ID=new_merchant),
+            transaction)
 
     def test_invalid_update_transaction(self):
         transaction = Transaction.objects.create(**self.valid_transaction)
-        transaction.amount = 0
-        with self.assertRaises(ValidationError):
-            Transaction.save()
+        transaction.transaction_amount = "one hundred"
+        with self.assertRaises(ValueError):
+            transaction.save()
 
     def test_delete_transaction(self):
         transaction = Transaction.objects.create(**self.valid_transaction)
@@ -51,15 +95,23 @@ class DefaultAccountTest(TestCase):
     """
 
     def setUp(self):
-        self.valid_account = {}
-        self.invalid_account = {}
+        create_fixtures()
+        self.valid_account = valid_account()
+        self.invalid_account = {
+            'account_status_code': AccountStatus.objects.get(account_status_desc='Active'),
+            'account_type_code': AccountType.objects.get(account_type_desc="Savings"),
+            'customer_id': '',
+            'account_number': '123-123-123000',
+            'current_balance': 200,
+            'other_details': '',
+        }
 
     def test_valid_create_account(self):
         new_account = Account.objects.create(**self.valid_account)
         self.assertIsInstance(new_account, Account)
 
     def test_invalid_create_account(self):
-        with self.assertRaises(ValidationError):
+        with self.assertRaises(ValueError):
             Account.objects.create(**self.invalid_account)
 
     def test_valid_update_account(self):
@@ -70,7 +122,7 @@ class DefaultAccountTest(TestCase):
 
     def test_invalid_update_account(self):
         account = Account.objects.create(**self.valid_account)
-        account.amount = 200
+        account.current_balance = "two hundred"
         with self.assertRaises(ValueError):
             account.save()
 
