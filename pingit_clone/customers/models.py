503b250b7ace86a2292c3d28b25c36f3c7f1db41
Hardy <hardiansyah.iiI@gmail.com>
2018-10-10 14:40:15 +0700

    override customer model save method to validate empty strings and add REQUIRED_FIELDS

--- pingit_clone/customers/models.py
+++ pingit_clone/customers/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from . import managers
@@ -18,6 +19,16 @@ class Customer(AbstractUser):
     contact_number = models.CharField(max_length=15, blank=False)
 
     objects = managers.CustomerManager()
+
+    REQUIRED_FIELDS = [
+        'email', 'first_name', 'last_name', 'identification_type',
+        'identification_number', 'contact_number'
+    ]
+
+    def save(self, *args, **kwargs):
+        if not self.identification_number:
+            raise ValidationError("Identication Number cannot be empty.")
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.email + ": " + self.first_name + " " + self.last_name
