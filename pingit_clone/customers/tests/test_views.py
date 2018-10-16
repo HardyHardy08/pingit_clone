b67951558c20926f5d36d8abf9b382623bb55d15
Hardy <hardiansyah.iiI@gmail.com>
2018-10-16 15:53:43 +0700

    add test_successful_signup_redirect_to_home_page_and_login and pass test

--- pingit_clone/customers/tests/test_views.py
+++ pingit_clone/customers/tests/test_views.py
@@ -1,9 +1,23 @@
+from django.contrib import auth
 from django.test import TestCase
+from django.urls import reverse
+
+from .test_models import valid_customer_data, setup_IdentificationTypes
 
 
 class CustomerViewsTest(TestCase):
-    def setUp(self):
-        pass
 
-    def test_redirect_to_home_page_after_success_signup(self):
-        pass
+    def setUp(self):
+        setup_IdentificationTypes()
+
+    def test_after_successful_signup_redirect_to_home_page_and_login(self):
+        data = valid_customer_data()
+        data['identification_type'] = "1"
+        data['password1'] = "123qwerty098"
+        data['password2'] = "123qwerty098"
+
+        response = self.client.post(reverse('customers:signup'), data=data)
+
+        user = auth.get_user(self.client)
+        self.assertRedirects(response, '/')
+        assert user.is_authenticated
--- pingit_clone/customers/views.py
+++ pingit_clone/customers/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -6,5 +8,11 @@ from .forms import CustomerCreationForm
 
 class SignUp(generic.CreateView):
     form_class = CustomerCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('home')
     template_name = 'signup.html'
+
+    def form_valid(self, form):
+        """If the form is valid, save the model, login user, and redirect to success URL."""
+        self.object = form.save()
+        login(self.request, self.object)
+        return HttpResponseRedirect(self.get_success_url())
