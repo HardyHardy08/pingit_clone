0f0725a9c6bce133a908d83550462eae72ff09e8
Hardy <hardiansyah.iiI@gmail.com>
2018-11-08 14:47:00 +0700

    refactor to use viewsets

--- pingit_clone/api/urls.py
+++ pingit_clone/api/urls.py
@@ -2,9 +2,16 @@ from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
 from api import views
 
+account_list = views.AccountViewSet.as_view({
+    'get': 'list',
+})
+account_detail = views.AccountViewSet.as_view({
+    'get': 'retrieve',
+})
+
 urlpatterns = [
-    path('account/<str:account_number>', views.AccountDetail.as_view(), name="account-detail"),
-    path('account/', views.AccountList.as_view(), name="account-list"),
+    path('account/<str:account_number>', account_detail, name="account-detail"),
+    path('account/', account_list, name="account-list"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- pingit_clone/api/views.py
+++ pingit_clone/api/views.py
@@ -1,21 +1,13 @@
-from rest_framework import generics, permissions
+from rest_framework import permissions, viewsets
 from banking.serializers import AccountSerializer
 from banking.models import Account
 from .permissions import IsOwner
 
 
-class AccountDetail(generics.RetrieveAPIView):
+class AccountViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = AccountSerializer
     lookup_field = 'account_number'
     permission_classes = (permissions.IsAuthenticated, IsOwner,)
-
-    def get_queryset(self):
-        return Account.objects.filter(customer_id=self.request.user)
-
-
-class AccountList(generics.ListAPIView):
-    serializer_class = AccountSerializer
-    permissions_classes = (permissions.IsAuthenticated,)
 
     def get_queryset(self):
         return Account.objects.filter(customer_id=self.request.user)
